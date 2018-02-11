68cb711462827080035fd41f7b567d8bec3c2a5f
Daniel Jilg <winsmith@winsmith.de>
2018-02-11 16:55:51 +0100

    Initial Commit

---
+++ Launch Pad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Launch Pad
+//
+//  Created by Daniel Jilg on 11.02.18.
+//  Copyright © 2018 breakthesystem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
